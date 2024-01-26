0321ed3288dbd5da2fab3c8bb05e378a57460dd6
Cuong Pham <cuong.pham@sea-solutions.com>
2024-01-26 10:32:38 +0700

    Added ChildModel

---
+++ test-workflow/ChildModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_workflow
+{
+    /// <summary>
+    /// A class for child objects.
+    /// </summary>
+    internal class ChildModel
+    {
+        public bool Growable { get; set; }
+    }
+}
