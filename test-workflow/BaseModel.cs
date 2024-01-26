ddaa44b2a5b65ff812cfd614a81afedb4c09b131
Cuong Pham <cuong.pham@sea-solutions.com>
2024-01-26 10:30:41 +0700

    Added comments

--- test-workflow/BaseModel.cs
+++ test-workflow/BaseModel.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace test_workflow
 {
+    /// <summary>
+    /// A base class for everything.
+    /// </summary>
     public class BaseModel
     {
         public int Id { get; set; }
