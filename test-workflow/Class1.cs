e52f45c61e75e3c0645f0d5456810596b7392b17
cuongph87 <152460338+cuongph87@users.noreply.github.com>
2024-05-02 15:57:57 +0700

    Dev new changes 1

--- test-workflow/Class1.cs
+++ test-workflow/Class1.cs
@@ -4,8 +4,8 @@
     {
 		// Main modification 1 before creating another branch
 		
-		// Main modification 2 modified after dev branch created
+		// Dev overwrote
 		
-		// Main modification 3 after creating dev branch
+		// Dev new changes 1
     }
 }
