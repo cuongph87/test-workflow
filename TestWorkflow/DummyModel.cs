701f5e2675a0c29f81ad33534d7406be80d2ddcc
cuongph87 <152460338+cuongph87@users.noreply.github.com>
2025-03-19 12:29:33 +0700

    f (#10)

---
+++ TestWorkflow/DummyModel.cs
@@ -0,0 +1,11 @@
+﻿namespace test_workflow;
+
+/// <summary>
+///     A new dummy model for testing DocFX
+/// </summary>
+public class DummyModel
+{
+    public int Id { get; set; }
+
+    public string Description { get; init; }
+}
